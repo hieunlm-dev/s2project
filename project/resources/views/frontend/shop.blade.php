4fb806cc6e0db8517f2c6b5f215309d6a1c447ce
Phan Hien <phanhien5634@gmail.com>
2021-08-16 22:31:34 +0700

    update

--- project/resources/views/frontend/shop.blade.php
+++ project/resources/views/frontend/shop.blade.php
@@ -26,7 +26,7 @@
 
 						<div class="wrap-right">
 							
-							<a href="/shop/{$id}?sort=ASC" class="btn btn-primary" role="button">Sort product ascending</a>
+							<a href="route('sortProduct')" class="btn btn-primary" role="button">Sort product ascending</a>
 
 							<div class="sort-item orderby ">
 								<select name="orderby" class="use-chosen" >
@@ -130,13 +130,6 @@
 					</div>
 
 					<div class="wrap-pagination-info">
-						{{-- <ul class="page-numbers">
-							<li><span class="page-number-item current" >1</span></li>
-							<li><a class="page-number-item" href="#" >2</a></li>
-							<li><a class="page-number-item" href="#" >3</a></li>
-							<li><a class="page-number-item next-link" href="#" >Next</a></li>
-						</ul>
-						<p class="result-count">Showing 1-8 of 12 result</p> --}}
 						<div class="d-flex justify-content-center">
 							{!! $products->links() !!}
 						</div>

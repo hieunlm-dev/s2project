4a87019c2faa90f0ad5b114e86161d5cc75d6ffe
hieunguyen195 <hieunguyen190594@gmail.com>
2021-08-22 09:54:26 +0700

    Update sidebar.blade.php

--- project/resources/views/admin/layout/partials/sidebar.blade.php
+++ project/resources/views/admin/layout/partials/sidebar.blade.php
@@ -107,7 +107,7 @@
             <li class="nav-item">
               <a href="{{ route('admin.brand.index') }}" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Brand</p>
+                <p>Brand List</p>
               </a>
             </li>
             <li class="nav-item">
